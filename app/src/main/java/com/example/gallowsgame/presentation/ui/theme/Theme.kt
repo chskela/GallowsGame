5c1b24d312ad2ec4a0e7790e64944ceecfb5a501
Aleksey Chistyakov <chskela@gmail.com>
2023-08-31 22:14:00 +0300

    refactor: change structure project

--- app/src/main/java/com/example/gallowsgame/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gallowsgame/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.gallowsgame.ui.theme
+package com.example.gallowsgame.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
--- app/src/main/java/com/example/gallowsgame/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/gallowsgame/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.gallowsgame.ui.theme
+package com.example.gallowsgame.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
