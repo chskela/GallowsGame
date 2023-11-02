9506e754084fd341cce56ddce066658a181d645f
Chistyakov Alexey <chskela@gmail.com>
2023-11-02 12:13:23 +0300

    fixed: fix adaptive mask design

--- app/src/main/java/com/chskela/gallowsgame/presentation/ui/components/mask/Mask.kt
+++ app/src/main/java/com/chskela/gallowsgame/presentation/ui/components/mask/Mask.kt
@@ -17,24 +17,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.coerceAtMost
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chskela.gallowsgame.presentation.ui.theme.GallowsGameTheme
 
 @Composable
 fun Mask(modifier: Modifier = Modifier, mask: String) {
     BoxWithConstraints(modifier = modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-        val sizeItem = (maxWidth - 6.dp * (mask.length - 1)) / mask.length
+        val sizeItem = ((maxWidth - 6.dp * (mask.length - 1)) / mask.length)
+//            .coerceAtMost(64.dp)
         Row(horizontalArrangement = Arrangement.Center) {
             mask.mapIndexed { index, letter ->
                 Box(
                     modifier = Modifier
-                        .sizeIn(maxWidth = sizeItem.coerceAtMost(64.dp))
+                        .sizeIn(maxWidth = sizeItem)
                         .aspectRatio(1f)
                         .background(color = MaterialTheme.colorScheme.primary),
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(text = letter.toString(), color = MaterialTheme.colorScheme.onPrimary)
+                    Text(
+                        text = letter.toString(),
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        fontSize = sizeItem.value.sp * 0.6
+                    )
                 }
                 if (index != mask.lastIndex) {
                     Spacer(modifier = Modifier.size(4.dp))
